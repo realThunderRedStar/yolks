27ce0c682cab5933596ec9fe6784f65ab18b8f20
Daniel Barton <danielb@purpleflaghosting.com>
2025-01-26 09:00:38 +0800

    Correct and improve postgres images and entrypoint.sh

--- postgres/entrypoint.sh
+++ postgres/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/ash
+#!/bin/bash
 cd /home/container
 
 #output current postgres version
@@ -8,9 +8,21 @@ postgres --version
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
+
+function wait-port {
+	local port=${1:-$SERVER_PORT}
+	until nc -z 127.0.0.1 $port ; do
+		sleep 1
+	done
+}
+
+
 # Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
 eval ${MODIFIED_STARTUP}
+
+# Ensure the server has a clean exit
+pg_ctl -D /home/container/postgres_db/ stop
