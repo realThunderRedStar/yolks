fc3e5d98ebd3373597e105e564046c4981ef4c80
Daniel Barton <danielb@purpleflaghosting.com>
2025-10-23 16:39:32 +0800

    NodeJS: Improve entrypoint

--- nodejs/entrypoint.sh
+++ nodejs/entrypoint.sh
@@ -6,7 +6,7 @@ INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
 # Print Node.js Version
-node -v
+echo "Node.js Version: $(node -v)"
 
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
